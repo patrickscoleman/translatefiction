a7f13ef538b633d5587f31438fd227ebf250a40a
Patrick Coleman <patrickscoleman@gmail.com>
2024-04-19 16:13:33 -0700

    use state in text boxes

--- app/components/textBox.tsx
+++ app/components/textBox.tsx
@@ -1,17 +1,27 @@
-const TextBox = ({ type }: { type: "source" | "target" }) => (
-  <div className="flex flex-col flex-1">
-    <textarea
-      className="resize-none p-4 flex-1 overflow-auto border rounded-md w-full"
-      placeholder={
-        type === "source"
-          ? "Enter text to translate..."
-          : "Editable translation appears here..."
-      }
-    ></textarea>
-    <p className="mt-2">
-      {type === "source" ? "Language: none detected" : "Language: English"}
-    </p>
-  </div>
-);
+"use client";
+
+import { useState } from "react";
+
+const TextBox = ({ type }: { type: "source" | "target" }) => {
+  const [text, setText] = useState("");
+
+  return (
+    <div className="flex flex-col flex-1">
+      <textarea
+        className="text-black resize-none p-4 flex-1 overflow-auto border rounded-md w-full"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder={
+          type === "source"
+            ? "Enter text to translate..."
+            : "Editable translation appears here..."
+        }
+      ></textarea>
+      <p className="mt-2">
+        {type === "source" ? "Language: none detected" : "Language: English"}
+      </p>
+    </div>
+  );
+};
 
 export default TextBox;
