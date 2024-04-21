6f1f1cbab4bb34518929cfefb2a5fcc97b685f2e
Patrick Coleman <patrickscoleman@gmail.com>
2024-04-20 17:22:43 -0700

    reduce padding around app

--- app/page.tsx
+++ app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
   };
 
   return (
-    <main className="flex flex-col min-h-screen p-16 overflow-hidden max-w-6xl mx-auto w-full">
+    <main className="flex flex-col min-h-screen p-8 overflow-hidden max-w-6xl mx-auto w-full">
       <h1 className="text-4xl font-bold mb-8 self-start">Translate Fiction</h1>
       <div className="flex flex-1 flex-col lg:flex-row gap-4 mb-4 w-full">
         <TextBox type="source" text={sourceText} setText={setSourceText} />
